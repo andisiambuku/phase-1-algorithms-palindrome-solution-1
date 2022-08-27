9fdc00b6be3a4230fd94d47fc7ee51f2f234f7e2
andisiambuku <roselandambuku@gmail.com>
2022-08-27 15:56:14 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+  //  // reverse the input string
+  //  const reversedWord = reverseString(word);
+  //  // compare the reversed string to the input
+  //  return word === reversedWord;
+}
+
+function reverseString(word) {
+  // // create an array from the input string
+  // const wordArray = word.split("");
+  // // reverse the array
+  // const reversedWordArray = wordArray.reverse();
+  // // create a string from the reversed array
+  // const reversedWord = reversedWordArray.join("");
+  // // return the reversed string
+  // return reversedWord;
+
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  create an array from the input string
+reverse the array
+create a string from the reversed array
+return the reversed string
 */
 
 // You can run `node index.js` to view these console logs
@@ -18,8 +53,29 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
 }
 
 module.exports = isPalindrome;
